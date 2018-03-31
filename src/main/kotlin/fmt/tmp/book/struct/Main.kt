23c385b810af87e8c25738f659f97c482b25018c
yo-kondo <buronson92@gmail.com>
2018-03-31 14:52:28 +0900

    完了

--- src/main/kotlin/fmt/tmp/book/struct/Main.kt
+++ src/main/kotlin/fmt/tmp/book/struct/Main.kt
@@ -9,13 +9,8 @@ import java.io.File
 fun main(args: Array<String>) {
     val conf = readConfig()
     val mdFileList = getMdFileList(conf.repositoryPath)
-
-    val mdList = mutableListOf<MarkDown>()
-    for (file in mdFileList) {
-        val md = MarkDown()
-        md.create(file)
-        mdList.add(md)
-    }
+    changeImageLink(mdFileList)
+    copyFile(mdFileList, conf.repositoryPath)
 }
 
 /**
@@ -45,4 +40,75 @@ private fun getMdFileList(repPath: String): List<File> {
         }
     }
     return rtnList
+}
+
+/**
+ * Markdown内の画像ファイルへのリンクを修正します。
+ */
+private fun changeImageLink(mdFileList: List<File>) {
+    for (mdFile in mdFileList) {
+        // 画像のディレクトリ
+        val imageDir = Regex("""^[0-9]{4}""").find(mdFile.name)?.value
+                ?: throw Exception("Markdownファイルの先頭に連番がついていません。filename=${mdFile.name}")
+
+        val text = mdFile.readText()
+                .replace("![](/img/$imageDir/", "![](")
+        mdFile.writeText(text)
+    }
+}
+
+/**
+ * ファイルをコピーします。
+ */
+private fun copyFile(mdFileList: List<File>, repPath: String) {
+    for (mdFile in mdFileList) {
+        val lines = mdFile.readLines()
+                .filter { it.startsWith("|") }
+
+        // 読了日（年）
+        val readingYear = lines
+                .first { it.contains("読了日") }
+                .split("|")[2]
+                .toDate().year
+        // ISBN
+        val isbn = getIsbn(lines)
+        // 画像のディレクトリ
+        val imageDir = Regex("""^[0-9]{4}""").find(mdFile.name)?.value
+                ?: throw Exception("Markdownファイルの先頭に連番がついていません。filename=${mdFile.name}")
+
+        // 画像コピー
+        val sourceImagePath = File("$repPath/$imageDir")
+        val targetImagePath = File("$repPath/md/$readingYear/$isbn/")
+        sourceImagePath.copyRecursively(targetImagePath, true)
+
+        // Markdownコピー
+        // ファイル名の先頭についている9999_を消す
+        val renameFile = mdFile.name.replace("${imageDir}_", "")
+        val targetPath = File("$repPath/md/$readingYear/$isbn/$renameFile")
+        mdFile.copyTo(targetPath, true)
+    }
+}
+
+/**
+ * ISBNを取得します。
+ */
+private fun getIsbn(lines: List<String>): String {
+    val isbn13 = lines
+            .first { it.contains("ISBN-13") }
+            .split("|")[2]
+            .let { if (it != "－") it else "" }
+    val isbn10 = lines
+            .first { it.contains("ISBN-10") }
+            .split("|")[2]
+            .let { if (it != "－") it else "" }
+    val asin = lines
+            .firstOrNull { it.contains("ASIN") }
+            ?.split("|")?.get(2)
+            .let { if (it != "－") it else "" }
+            ?: ""
+    return when {
+        isbn13.isNotEmpty() -> isbn13
+        isbn10.isNotEmpty() -> isbn10
+        else -> asin
+    }
 }
